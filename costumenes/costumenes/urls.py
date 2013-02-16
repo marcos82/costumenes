225cadc4983f1758d56f2cfbd5a621c945bdb943
MarcoS <marc.sedda@gmail.com>
2013-02-16 17:42:21 +0100

    fix identazione urls.py

--- costumenes/costumenes/urls.py
+++ costumenes/costumenes/urls.py
@@ -8,11 +8,11 @@ urlpatterns = patterns('',
 
 
 	('r^getAllComune/$','sardinia.views.getAllComune'), 					#tutti i comuni
-    	('r^getAllPeolpe/$','sardinia.views.getAllPeople'), 					#tutte le persone
-    	('r^getAllPhoto/$','sardinia.views.getAllPhoto'),  					#tutte le foto
-    	('r^getComuniProvincia/(?P<provincia>\w+)','sardinia.views.getComuneProvincia'), 	#comuni di una provincia
+    ('r^getAllPeolpe/$','sardinia.views.getAllPeople'), 					#tutte le persone
+    ('r^getAllPhoto/$','sardinia.views.getAllPhoto'),  					#tutte le foto
+    ('r^getComuniProvincia/(?P<provincia>\w+)','sardinia.views.getComuneProvincia'), 	#comuni di una provincia
 	('r^getComuniRegione/(?P<regione>\w+)','sardinia.views.getComuneRegione'),		#Comuni di una regione
-    	('r^getComune/(?P<comune_id>\w+)','sardinia.views.getComune'), 				#singolo comune
+    ('r^getComune/(?P<comune_id>\w+)','sardinia.views.getComune'), 				#singolo comune
 	('r^getPhotoComune/(?P<comune>\w+)/$','sardinia.views.getPhotoComune'),			#foto di un comune
 	('r^getPhotoType/(?P<type>)/$$','sardinia.views.getPhotoType'),
 
