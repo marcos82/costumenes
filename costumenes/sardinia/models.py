ee314b9817218ef0eb0fded325fff9bdc65ae6b3
MarcoS <marc.sedda@gmail.com>
2013-02-16 17:13:59 +0100

    test commit

--- costumenes/sardinia/models.py
+++ costumenes/sardinia/models.py
@@ -1,6 +1,10 @@
 import os
 import uuid
 
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+from imagekit.processors import resize, Adjust, Crop
+
 from django.db import models
 
 # A list of sardinian's regions
@@ -56,9 +60,12 @@ COSTUME_TIPOLOGY = (
 )
 
 #This function will return the path based on the "comune" and "costume" value
-def original_directory_generator(instance, path):                
-        return 'photos/%s/%s/%s.jpg' %(instance.comune.nome.lower(),
+def original_directory_generator(instance, path, thumb):   
+	if thumb:
+		return 'photos/%s/%s/thumb/%s.jpg' %(instance.comune.nome.lower(),
         instance.tipologia.lower(), uuid.uuid4())
+        
+	return 'photos/%s/%s/%s.jpg' %(instance.comune.nome.lower(), instance.tipologia.lower(), uuid.uuid4())
 
 class Comune(models.Model):
 	nome = models.CharField(max_length=100)
@@ -73,25 +80,28 @@ class Comune(models.Model):
 		verbose_name_plural = "comuni"
 
 class Photo(models.Model):
-	comune = models.ForeignKey(Comune)
-	sesso = models.CharField(max_length=20, choices=COSTUME_SEX)
-	tipologia = models.CharField(max_length=50, choices=COSTUME_TIPOLOGY)
-	img = models.ImageField("path immagine", upload_to=original_directory_generator)
-	titolo = models.CharField(max_length=100)
-	descrizione = models.TextField()
-	data_scatto = models.DateTimeField()
-	data_inserimento = models.DateTimeField(auto_now_add=True)
-	autore_scatto = models.CharField(max_length=100, null=True, blank=True)
-	localita_scatto = models.CharField(max_length=150, null=True, blank=True)
-	nome_cognome_modello = models.CharField(max_length=100, null=True, blank=True)
-	foto_copertina = models.CharField(max_length=2, choices=SI_NO, default="No")
-	
-	def __unicode__(self):
-		return "%s - %s - %s" % (self.comune, self.tipologia.replace("_", " "), self.titolo)
-    
-	class Meta:
-		verbose_name_plural = "foto"
+    comune = models.ForeignKey(Comune)
+    sesso = models.CharField(max_length=20, choices=COSTUME_SEX)
+    tipologia = models.CharField(max_length=50, choices=COSTUME_TIPOLOGY)
+    img = models.ImageField("path immagine", upload_to=original_directory_generator(False))
+    thumbnail = ImageSpecField(source='img', processors=[Crop(100, 50)], format='JPEG', options={'quality': 90}
+        , cache_to=original_directory_generator(True))
+    #thumbnail = ImageSpec([Adjust(contrast=1.2, sharpness=1.1),
+    #        resize.Crop(50, 50)], image_field='img',
+    #        format='JPEG', quality=90, cache_to=original_directory_generator(true))
+    num_views = models.PositiveIntegerField(editable=False, default=0)
+    titolo = models.CharField(max_length=100)
+    descrizione = models.TextField()
+    data_scatto = models.DateTimeField()
+    data_inserimento = models.DateTimeField(auto_now_add=True)
+    autore_scatto = models.CharField(max_length=100, null=True, blank=True)
+    localita_scatto = models.CharField(max_length=150, null=True, blank=True)
+    nome_cognome_modello = models.CharField(max_length=100, null=True, blank=True)
+    foto_copertina = models.CharField(max_length=2, choices=SI_NO, default="No")
 
-	
+    def __unicode__(self):
+        return "%s - %s - %s" % (self.comune, self.tipologia.replace("_", " "), self.titolo)
 
+    class Meta:
+        verbose_name_plural = "foto"
 
